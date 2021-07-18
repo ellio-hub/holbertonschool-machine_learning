4cd6abceed67c1903206479dd98fa47f9132b560
vito <mansourali1919@gmail.com>
2021-07-18 19:12:33 +0100

    first push

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    10-ill_use_my_scale module
+"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """ function that calculates the shape of a numpy.ndarray
+
+    Returns:
+        [tuple]: shape of an array
+    """
+    return np.shape(matrix)
