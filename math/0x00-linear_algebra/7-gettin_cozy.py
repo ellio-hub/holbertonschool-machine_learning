7e3495a4f7b7b386a781176a7b7986b5720aec60
vito <mansourali1919@gmail.com>
2021-07-18 15:48:40 +0100

    push

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -14,13 +14,19 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return None
     if axis == 1 and len(mat1) != len(mat2):
         return None
+    new = []
     for i in range(len(mat1)):
         if len(mat1[i]) == 0:
             return None
-    new = mat1.copy()
+        new.append(mat1[i])
     if axis == 1:
         for i in range(len(mat2)):
-            new[i].extend(mat2[i])
+            for j in range(len(mat2[0])):
+                new[i].append(mat2[i][j])
+        print(hex(id(new)))
+        print(hex(id(mat1)))
     else:
         new.extend(mat2)
+    print(new)
+    print(mat1)
     return new
