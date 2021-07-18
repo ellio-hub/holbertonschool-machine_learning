e4f7d5b5072963178cef5e8b21ca343a031ce026
vito <mansourali1919@gmail.com>
2021-07-18 14:24:10 +0100

    fix doc...

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
+"""
+    7-getting_cozy
+"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis
+
+    Returns:
+        [list]: concatinated matrices
+    """
+    for i in range(len(mat1)):
+        if mat1[i] == []:
+            return None
     new = mat1.copy()
     if axis == 1:
         for i in range(len(mat2)):
+            if mat2[i] == []:
+                return None
             new[i].extend(mat2[i])
     else:
         new.extend(mat2)
