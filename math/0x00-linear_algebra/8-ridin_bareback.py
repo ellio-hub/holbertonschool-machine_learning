891492e401727170e19c6e238d863e9dfa9d58ef
vito <mansourali1919@gmail.com>
2021-07-18 16:47:25 +0100

    fix doc ...

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
+"""
+    8-ridin_bareback module
+"""
+
+
 def mat_mul(mat1, mat2):
-    
+    """ function that performs matrix multiplication
+
+    Returns:
+        [list]: multiplied matrices
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+    new = []
+    for i in range(len(mat1)):
+        new.append([])
+        for j in range(len(mat2[0])):
+            d = 0
+            for x in range(len(mat1[0])):
+                d = d + (mat1[i][x] * mat2[x][j])
+            new[i].append(d)
+    return new
