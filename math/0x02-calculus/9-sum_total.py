fc74419619b78cb764f91775c4f1f768d682425e
vito <mansourali1919@gmail.com>
2021-07-22 22:42:51 +0100

    1st push

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -8,4 +8,6 @@ def summation_i_squared(n):
     """
         function that calculated sum of an int
     """
+    if type(n) is not int and n < 0:
+        return None
     return int((n * (n + 1) * (2 * n + 1)) / 6)
