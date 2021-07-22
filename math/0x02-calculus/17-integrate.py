375e7a21b12a81a1d639076d80c836cf9e5f0ce9
vito <mansourali1919@gmail.com>
2021-07-23 00:41:36 +0100

    1st push

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    17-integrate module
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+        function that calculates the integral of a polynomial
+    """
+    if type(poly) != list or len(poly) == 0 or type(C) != int:
+        return None
+    x = []
+    x.append(C)
+    for i in range(len(poly)):
+        if type(poly[i]) != int:
+            return None
+        if (poly[i] / (i + 1)) % 1 == 0:
+            x.append(int(poly[i] / (i + 1)))
+        else:
+            x.append(poly[i] / (i + 1))
+    return x
