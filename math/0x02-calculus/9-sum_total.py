cab4f85334f784887a17dac200173d962b0c5464
vito <mansourali1919@gmail.com>
2021-07-22 22:37:35 +0100

    1st push

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    9-sum_total module
+"""
+
+
+def summation_i_squared(n):
+    """
+        function that calculated sum of an int
+    """
+    return int((n * (n + 1) * (2 * n + 1)) / 6)
